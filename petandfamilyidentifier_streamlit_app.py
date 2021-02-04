4a020e4a8bb95b10c934e36f2e9bcbfe6ad851d6
Jonathan Martin <jonamar10@gmail.com>
2021-02-04 14:35:41 -0300

    Streamlit app for pet and family identifier v2

---
+++ petandfamilyidentifier_streamlit_app.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 08:45:45 2021
+
+@author: Jonx-PC
+"""
+from pathlib import Path
+from fastai.vision.all import *
+from fastai.vision.widgets import *
+import wandb 
+import streamlit as st
+import numpy as np
+import pandas as pd
+from fastai.callback.wandb import *
+set_seed(7)
+
+def get_x(r): return path/r['fname']
+def get_y(r): return r['labels'].split(' ')
+
+
+learn = load_learner('petandfamilyidentifier_windows.pkl', cpu=True)
+
+st.set_page_config(layout="wide")
+st.title('Pet and Family identifier V2')
+st.header('Summary')
+st.markdown('The app was consibed with the idea of auto identifier my family\'s pets:heart_eyes:, and also some family members.:sweat_smile:')
+st.markdown('This version of the app has a facewhashing powered by Streamlit.')
+st.markdown('Finally, it\'s include new funcionality to handle multiple images at the same time. Also a section with examples bellow.')
+
+uploaded_files = st.file_uploader("Upload your photos", type = ['jpg','jpeg','png'],
+                                  accept_multiple_files=True)
+
+photo_container = st.beta_expander("Results",expanded = True)
+    
+with photo_container:
+    col1, col2 = st.beta_columns(2)
+    for uploaded_file in uploaded_files:
+        with st.beta_container():
+            col1, col2 = st.beta_columns(2)
+            bytes_data = uploaded_file.read()
+            img = PILImage.create(bytes_data)
+            col1.image(img, use_column_width=True)
+            pred,pred_idx,probs = learn.predict(img)
+            col2.subheader('In this photo the model found:')
+            for i in range(len(pred)):
+                col2.markdown(f'**{pred[i].capitalize()}** with a probability of: **{probs[pred_idx][i]*100:.00f}%**')
+            # col2.subheader('Is there an error? Select it please:')
+            # for i in range(len(pred)):
+            #     an_error = col2.checkbox(f'{pred[i].capitalize()} is not in the photo')
+            #     if an_error:
+
+
+def show_example(image_path, container):
+    with container:
+        img = PILImage.create(image_path)
+        st.image(img, use_column_width=True)
+        pred,pred_idx,probs = learn.predict(img)
+        st.subheader('In this photo the model found:')
+        for i in range(len(pred)):
+            st.markdown(f'**{pred[i].capitalize()}** with a probability of: **{probs[pred_idx][i]*100:.00f}%**')
+
+examples = st.beta_container()
+with examples:
+    st.header('Examples')
+    examples_containers = st.beta_columns(3)
+    examples_images = ['./new_sol.jpg', './new_nexus.jpeg', './new_nexus_jony.jpeg']
+    for example in range(len(examples_images)):
+        show_example(examples_images[example], examples_containers[example])
+    
+    
